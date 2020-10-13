b85ef9a842fc572a5a38a69bbc31a224c94f403d
carts123 <72449546+carts123@users.noreply.github.com>
2020-10-13 14:53:55 +0100

    I applied, this commit will have removed all existing code and added four Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-favoriteFood = porridge
-document.write('porridge') 
+let pokemonList = [
+  { name: 'Charmander', height: 0.6, types: ['steel', 'fire', 'ice']}
+  { name: 'Jigglypuff', height: 0.5, types: ['bug', 'dark', 'ghost']}
+  { name: 'Pidgeot', height: 1.5m, types: ['rock', 'electric', 'ice']}
+  { name: 'Psyduck', height: 0.8m, types: ['grass', 'electric', 'fire']}
