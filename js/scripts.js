c742ce1141d77c13c0900567de31c9c3acbcd6ef
carts123 <72449546+carts123@users.noreply.github.com>
2020-10-13 14:58:44 +0100

    Closed end bracket

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,4 @@ let pokemonList = [
   { name: 'Jigglypuff', height: 0.5, types: ['bug', 'dark', 'ghost']}
   { name: 'Pidgeot', height: 1.5m, types: ['rock', 'electric', 'ice']}
   { name: 'Psyduck', height: 0.8m, types: ['grass', 'electric', 'fire']}
+]
