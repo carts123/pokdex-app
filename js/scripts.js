fb6c644211c5e4fd2b816ca5b506d797338e7c66
carts123 <72449546+carts123@users.noreply.github.com>
2020-10-17 19:06:55 +0100

    If applied, this commit will have created a loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ let pokemonList = [
   { name: 'Pidgeot', height: 1.5m, types: ['rock', 'electric', 'ice']}
   { name: 'Psyduck', height: 0.8m, types: ['grass', 'electric', 'fire']}
 ]
+//lists pokemon name and height
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + pokedexList[i].types)
+  if (pokemonList[i].height >= 1.0) {
+    document.write(pokemonList[i].height + "Wow, that’s big!")
+  }
+  else if (pokemonList[i].height)
+}
