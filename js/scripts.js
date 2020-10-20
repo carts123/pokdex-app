25a8920c17d9ed2dfa44b0e82f48785d8fb8332f
carts123 <72449546+carts123@users.noreply.github.com>
2020-10-20 22:52:08 +0100

    If applied, this commit will have corrected further syntax errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
-  { name: 'Charmander', height: 0.6, types: ['steel', 'fire', 'ice']}
-  { name: 'Jigglypuff', height: 0.5, types: ['bug', 'dark', 'ghost']}
-  { name: 'Pidgeot', height: 1.5m, types: ['rock', 'electric', 'ice']}
-  { name: 'Psyduck', height: 0.8m, types: ['grass', 'electric', 'fire']}
+  { name: 'Charmander', height: 0.6, types: ['steel', 'fire', 'ice']},
+  { name: 'Jigglypuff', height: 0.5, types: ['bug', 'dark', 'ghost']},
+  { name: 'Pidgeot', height: 1.5, types: ['rock', 'electric', 'ice']},
+  { name: 'Psyduck', height: 0.8, types: ['grass', 'electric', 'fire']},
 ]
 //lists pokemon name and height
 for (let i = 0; i < pokemonList.length; i++) {
