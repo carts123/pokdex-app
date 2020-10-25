471a872f519ece4d9624f1beec4d6875411a511d
carts123 <72449546+carts123@users.noreply.github.com>
2020-10-25 11:09:41 +0000

    I applied, this commit will have wrapped the pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
+let pokemonRepository = (function () {
 let pokemonList = [
   { name: 'Charmander', height: 0.6, types: ['steel', 'fire', 'ice']},
   { name: 'Jigglypuff', height: 0.5, types: ['bug', 'dark', 'ghost']},
   { name: 'Pidgeot', height: 1.5, types: ['rock', 'electric', 'ice']},
   { name: 'Psyduck', height: 0.8, types: ['grass', 'electric', 'fire']},
-]
-//lists pokemon name and height
-pokemonList.forEach(function(pokemon) {
+];
+
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Squirtle', height: 0.5, types: ['water']});
+console.log(pokemonRepository.getAll());
+
+pokemonList.forEach(function(add) {
   document.write((pokemon.name + " " + pokemon.height +"<br/>"));
 });
 
